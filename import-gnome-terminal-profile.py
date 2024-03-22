a85a806daa14289030ba3d5d852e37c7179399d0
Gary Danko <gdanko@protonmail.com>
2024-03-22 13:19:56 -0700

    use common/; add functionality to check if a profile exists before adding it; verify the profile was properly created at the end; sanitize error messages

--- import-gnome-terminal-profile.py
+++ import-gnome-terminal-profile.py
@@ -5,7 +5,7 @@ import argparse
 import uuid
 import sys
 import os
-# from subprocess import Popen, PIPE, STDOUT
+import common.util
 import subprocess
 
 def configure():
@@ -15,30 +15,8 @@ def configure():
     args = parser.parse_args()
     return args
 
-def execute_command(command=[], input=''):
-    foo = input.split('\n')
-    process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate(input='\n'.join(foo).encode('utf-8'))
-
-    return process.returncode, stderr.decode('utf-8').strip(), stdout.decode('utf-8').strip()
-
-def get_current_profiles():
-    profiles_path = '/org/gnome/terminal/legacy/profiles:/'
-    command = ['dconf', 'list', profiles_path]
-    returncode, stderr, stdout = execute_command(command=command)
-
-    if returncode != 0:
-        print(f'An error has occurred: {stderr}')
-        exit(1)
-
-    profiles = stdout.split('\n')
-    profiles = [profile.lstrip(':').rstrip('/') for profile in profiles if profile.startswith(':') and profile.endswith('/')]
-
-    return profiles
-
 def read_input(args):
     profile_id = str(uuid.uuid4())
-    pofile_contents = None
     try:
         with open(args.input) as f: contents = f.read()
         return profile_id, contents.strip()
@@ -55,24 +33,44 @@ def create_new_profile(profiles_list, profile_id, profile_contents):
     profile_list_path = '/org/gnome/terminal/legacy/profiles:/list'
     dconf_input = '[' + ','.join(["'"+item+"'" for item in profiles_list]) + ']'
     command = ['dconf', 'write', profile_list_path, dconf_input]
-    returncode, stdout, stderr = execute_command(command)
+    returncode, stdout, stderr = common.util.execute_command(command)
 
     if returncode != 0:
-        print(f'Failed to update {profile_list_path}: {stderr}')
-        exit(1)
+        print(f'Failed to update {profile_list_path}: {common.util.sanitize_error(stderr)}')
+        sys.exit(1)
 
 def main():
     args = configure()
     if not (os.path.exists and os.path.isfile(args.input)):
-        print(f'The path {args.input} does not exist or is not a file')
-        sys.exit(1)
+        print(f'The path {args.input} does not exist or is not a file.')
+        return 1
 
-    profiles_list = get_current_profiles()
+    profiles_dict = common.util.get_current_profiles()
+    profiles_list = [value for _, value in profiles_dict.items()]
+    profile_name = os.path.splitext(os.path.basename(args.input))[0]
+
+    if profile_name in profiles_dict:
+        print(f'The profile name {profile_name} already exists. Please select another name and try again.')
+        return 0
+
     profile_id, profile_contents = read_input(args)
     if profile_id is not None and profile_contents is not None:
         profiles_list.append(profile_id)
         create_new_profile(profiles_list, profile_id, profile_contents)
 
+    # Verify the profile was created
+    profiles_dict = common.util.get_current_profiles()
+    enabled = common.util.get_enabled_list()
+    if profile_name in profiles_dict and profiles_dict[profile_name] == profile_id:
+        print(f'The Gnome Terminal profile {profile_name} was successfully created.')
+        if profile_id in enabled:
+            print(f'The Gnome Terminal profile {profile_name} was successfully added to the enabled list.')
+        else:
+            print(f'The Gnome Terminal profile {profile_name} was NOT added to the enabled list.')
+            return 1
+    else:
+        print(f'The Gnome Terminal profile {profile_name} was NOT created.')
+        return 1
 
 if __name__ == '__main__':
     sys.exit(main())
